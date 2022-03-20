fbea8ae8b27a1a66a9af3307ccc7218a0b583760
santonow <stas.antonowicz@gmail.com>
2022-03-20 10:38:18 +0100

    Some changes in correlation inference and new script for collecting known interactions.

---
+++ utils/collect_known_interactions.py
@@ -0,0 +1,108 @@
+import csv
+import json
+import subprocess
+import zipfile
+
+import pandas as pd
+import networkx as nx
+
+
+manual_mapping = {
+    'Diatomea': 'Bacillariophyta',
+    'Ancyromonadidae': 'Planomonadida',
+    'Bolidomonas': 'Triparma',
+    'Carpediemonas-like': 'Carpediemonas',
+    'Centrohelida': 'Centroheliozoa',
+    'Choanomonada': 'Choanoflagellida',
+    'Incertae_sedis_rhizaria': 'Rhizaria',
+    'Incertae_sedis_stramenopilees': 'Stramenopiles',
+    'Incertae_sedis_stramenopiles': 'Stramenopiles',
+    'unknown_alveolate': 'Alveolata',
+    'unknown_amoebozoan': 'Amoebozoa',
+    'unknown_eukaryote': 'Eukaryota',
+    'Eukaryote': 'Eukaryota',
+    'unknown_rhizarian': 'Rhizaria',
+    'Telonema_subtilis': 'Telonema_subtile',
+}
+
+
+def group_to_X(s):
+    splitted = s.split('_')
+    if len(splitted) < 3:
+        return s
+
+    if splitted[-2] == 'Group':
+        return '_'.join(splitted[:-2]) + '_' + 'X' * len(splitted[-1])
+
+
+def match_lineage(lineage, lineages):
+    options = []
+    if str(lineage[-1]) != 'nan':
+        options.append('_'.join([lineage[-2], lineage[-1]]))
+        if options[0] in manual_mapping:
+            options.append(manual_mapping[options[0]])
+        if lineage[-2] in manual_mapping:
+            options.append('_'.join([manual_mapping[lineage[-2]], lineage[-1]]))
+        species_group_t_X = group_to_X(lineage[-1])
+        if species_group_t_X != lineage[-1]:
+            options.append(species_group_t_X)
+    options.append(lineage[-2])
+    if lineage[-2] in manual_mapping:
+        options.append(manual_mapping[lineage[-2]])
+    genera_group_t_X = group_to_X(lineage[-2])
+    if genera_group_t_X != lineage[-2]:
+        options.append(genera_group_t_X)
+    for option in options:
+        if option.lower() in lineages:
+            return lineages[option.lower()]
+
+
+PIDA_URL = 'https://github.com/ramalok/PIDA/archive/refs/tags/v1.08.zip'
+
+status = subprocess.run(['wget', PIDA_URL, '-O', snakemake.output[0]])
+
+lineages = dict()
+with open(snakemake.input[0], 'r') as handle:
+    reader = csv.reader(handle, delimiter='\t')
+    next(reader)
+    for _, *lineage in reader:
+        for i, elem in enumerate(lineage):
+            if elem:
+                lineages[elem.lower()] = lineage[:i+1]
+
+
+with zipfile.ZipFile(snakemake.output[0]) as zfile:
+    for fname in zfile.filelist:
+        if fname.filename.endswith('.xlsx'):
+            with zfile.open(fname) as handle:
+                df = pd.read_excel(handle, header=2)
+
+
+relations = nx.DiGraph()
+for record in df.to_dict(orient='records'):
+    lineage_left = [
+        record[f'Taxonomic level {i}: org1'] for i in range(1, 4)
+    ] + [record['Genus org1'], record['Species org1']]
+    lineage_right = [
+        record[f'Taxonomic level {i}: org2'] for i in range(1, 4)
+    ] + [record['Genus org2'], record['Species org2']]
+    if any(x[0] == 'Prokaryote' for x in [lineage_left, lineage_right]):
+        continue
+    left_match = match_lineage(lineage_left, lineages)
+    right_match = match_lineage(lineage_right, lineages)
+
+    if left_match is not None and right_match is not None:
+        relations.add_edge(
+            tuple(left_match), tuple(right_match), interaction=record['Interaction'],
+            ecological_interaction=record['Ecological interaction'], habitat=record['Habitat']
+        )
+
+with open(snakemake.output[1], 'w') as handle:
+    writer = csv.writer(handle, delimiter='\t')
+    writer.writerow(['head', 'tail', 'interaction', 'ecological_interaction', 'habitat'])
+    for head, tail, attrs in relations.edges(data=True):
+        writer.writerow([
+            json.dumps(head), json.dumps(tail), *[
+                attrs[field_name] for field_name in ['interaction', 'ecological_interaction', 'habitat']
+            ]
+        ])
--- utils/run_correlation_inference.py
+++ utils/run_correlation_inference.py
@@ -2,16 +2,17 @@ import csv
 import os
 import sys
 from collections import Counter
-from typing import List, Union
+from typing import List, Union, Tuple, Set
 
 from joblib import Parallel
-from joblib import delayed as _delayed
+from joblib import delayed as delayed
 import numpy as np
 import networkx as nx
 
 from occurence_table import OTUTable
 from numba_functions import compute_correlations, compute_p_value, reboot, compute_pvals, \
-    update_mean_variance_parallel, normalize, benjamini_hochberg, merge_p_values, clr, write_matrix
+    update_mean_variance_parallel, normalize, benjamini_hochberg, merge_p_values, clr, write_matrix, \
+    matrix_iter
 
 
 class timer:
@@ -27,134 +28,190 @@ class timer:
         print()
 
 
-def delayed(fun):
-    return _delayed(fun)
+class CoNet:
 
-
-@delayed
-def permuter(matrix: np.ndarray, proc_iters: int, method: str, proc_num: int, tmp_dir: str):
-    try:
-        result = reboot(matrix, proc_iters, method, proc_num=proc_num, tmp_dir=tmp_dir)
-    except Exception as e:
-        print(matrix.shape, proc_iters, method)
-        print(e)
-        raise e
-    return result
-
-
-def compute_pvals_parallel(matrix: np.ndarray, n_iter: int, method: str, n_jobs: int, tmp_dir: str):
-    def prepare_proc_iters(n_iter, n_jobs):
-        iters = [n_iter // n_jobs for _ in range(n_jobs)]
-        iters[-1] += n_iter - sum(iters)
-        return iters
-
-    executor = Parallel(n_jobs=n_jobs, verbose=10)
-    tasks = (
-        permuter(matrix, proc_iters, method, i, tmp_dir)
-        for i, proc_iters in enumerate(prepare_proc_iters(n_iter, n_jobs))
-    )
-
-    current_count = 0
-    perm_means = np.zeros((matrix.shape[1], matrix.shape[1]))
-    perm_variances = np.zeros((matrix.shape[1], matrix.shape[1]))
-    bs_means = np.zeros((matrix.shape[1], matrix.shape[1]))
-    bs_variances = np.zeros((matrix.shape[1], matrix.shape[1]))
-    samples = []
-    for result in executor(tasks):
-        curr_perm_means, curr_perm_variances, curr_bs_means, curr_bs_variances, count, proc_samples = result
-        perm_means, perm_variances = update_mean_variance_parallel(
-            current_count, perm_means, perm_variances,
-            count, curr_perm_means, curr_perm_variances
-        )
-        bs_means, bs_variances = update_mean_variance_parallel(
-            current_count, bs_means, bs_variances,
-            count, curr_bs_means, curr_bs_variances
-        )
-        current_count += count
-        samples.extend(proc_samples)
-    perm_variances /= current_count
-    bs_variances /= current_count
-    assert current_count == n_iter
-
-    return compute_p_value(perm_means, perm_variances, bs_means, bs_variances), samples
-
-
-def infer_network(
-    otu_table: OTUTable, n_iter: int, methods: Union[str, List[str]], tmp_dir: str,
-    p_value_threshold: float = 0.05, n_jobs: int = 4, bh: bool = True, confidence_interval=0.95,
-) -> nx.Graph:
-    network = nx.Graph()
-    os.makedirs(tmp_dir, exist_ok=True)
-    indice_to_id = {i: _id for i, _id in enumerate(otu_table.ids(axis='observation'))}
-    if isinstance(methods, str):
-        methods = [methods]
-    results = dict()
-    for method in methods:
-        if method != 'kullback-leibler':
-            with timer(f'Computing clr transformation for method {method}...'):
-                matrix = clr(otu_table.matrix.T)
-        else:
-            matrix = otu_table.matrix.T
-        if method == 'kullback-leibler':
-            with timer('Normalizing...'):
-                normalize(matrix)
-        with timer(f'Computing correlations with method {method}...'):
-            correlations = compute_correlations(matrix, method)
-        write_matrix(tmp_dir, correlations, f'correlations_{method}')
-        with timer(f'Computing p-values for method {method} using {n_jobs} processes...'):
-            if n_jobs == 1:
-                pvalues, bs_samples = compute_pvals(matrix, n_iter, method, tmp_dir)
-            else:
-                pvalues, bs_samples = compute_pvals_parallel(matrix, n_iter, method, n_jobs)
-        write_matrix(tmp_dir, pvalues, f'pvalues_{method}')
-        left_interval, right_interval = np.percentile(
-            np.array(bs_samples),
-            [100 * (1 - confidence_interval) / 2, 100 * (1 - (1 - confidence_interval) / 2)]
-        )
-        pvalues[pvalues > 0.5] = 1 - pvalues[pvalues > 0.5]  # we are doing one-sided test
-        results[method] = {
-            'correlations': correlations,
-            'pvalues': pvalues,
-            'confidence_interval': (left_interval, right_interval)
+    def __init__(
+        self, otu_table: OTUTable, n_iter: int = 1000,
+        methods: Tuple[str] = ('spearman', 'kullback-leibler'),
+        p_value_threshold: float = 0.05, n_jobs: int = 4, bh: bool = True,
+        confidence_interval: float = 0.95, n_initial_edges: int = 1000000
+    ):
+        self.otu_table = OTUTable
+        self.indice_to_id = {
+            i: _id for i, _id in enumerate(otu_table.ids(axis='observation'))
         }
-    if len(results) > 1:
-        with timer('Merging p values...'):
-            pvals = merge_p_values(*[result['pvalues'] for result in results.values()])
-    else:
-        pvals = next(iter(results.values()))['pvalues']
-    if bh:
-        with timer('Performing Benjamini-Hochberg correction...'):
-            pvals = benjamini_hochberg(pvals)
-    write_matrix(tmp_dir, pvals, 'final_pvals')
-    rejected_edges = Counter()
-    accepted_edges = Counter()
-    for method, result in results.items():
-        correlations = result['correlations']
-        left_interval, right_interval = result['confidence_interval']
-        method_pvalues = result['pvalues']
-        for i in range(correlations.shape[0]):
-            for j in range(correlations.shape[1]):
-                if i != j and i < j:
-                    if (
-                        pvals[i, j] < p_value_threshold
-                        and (left_interval < correlations[i, j] < right_interval)
-                    ):
-                        left_otu_id = indice_to_id[i]
-                        right_otu_id = indice_to_id[j]
-                        if not network.has_edge(left_otu_id, right_otu_id):
-                            network.add_edge(
-                                left_otu_id, right_otu_id,
-                                pvalue=pvals[i, j],
-                            )
-                            accepted_edges[method] += 1
-                        network[left_otu_id][right_otu_id][f'{method}_weight'] = correlations[i, j]
-                        network[left_otu_id][right_otu_id][f'{method}_pvalue'] = method_pvalues[i, j]
-                    else:
-                        rejected_edges[method] += 1
-    print('Accepted edges:', accepted_edges)
-    print('Rejected edges:', rejected_edges)
+        self.matrix = otu_table.matrix.T
+        self.n_iter = n_iter
+        self.methods = methods
+        self.p_value_threshold = p_value_threshold
+        self.n_jobs = n_jobs
+        self.bh = bh
+        self.confidence_interval = confidence_interval
+        self.n_initial_edges = n_initial_edges
+        self.matrices_by_method = self.prepare_matrices()
+        by_method, common_edges = self.initial_correlation()
+        self.correlations_by_method = by_method
+        self.common_edges = common_edges
+        self.results = None
 
-    return network
+    def prepare_matrices(self):
+        matrices = dict()
+        for method in self.methods:
+            if method != 'kullback-leibler':
+                with timer(f'Computing clr transformation for method {method}...'):
+                    matrix = clr(self.matrix)
+            else:
+                matrix = self.matrix.copy()
+            if method == 'kullback-leibler':
+                with timer('Normalizing...'):
+                    normalize(matrix)
+            matrices[method] = matrix
+        return matrices
+
+    def initial_correlation(self):
+        all_edges = []
+        correlations_by_method = dict()
+        for method, matrix in self.matrices_by_method.items():
+            with timer(f'Computing correlations with method {method}...'):
+                correlations = compute_correlations(matrix, method)
+            correlations_by_method[method] = correlations
+            edges, left, right = self.select_edges(correlations, method, self.n_initial_edges)
+            all_edges.append(edges)
+            print(f'Selecting {self.n_initial_edges} initial edges, metric value < {left} and > {right}')
+        common_edges = set.intersection(*all_edges)
+        print(f'Selecting {len(common_edges)} common initial edges.')
+        return correlations_by_method, common_edges
+
+    def run(self):
+        results = dict()
+        for method, matrix in self.matrices_by_method.items():
+            with timer(f'Computing p-values for method {method} using {self.n_jobs} processes...'):
+                if self.n_jobs == 1:
+                    pvalues, bs_samples = compute_pvals(matrix, self.n_iter, method, self.common_edges)
+                else:
+                    pvalues, bs_samples = self.compute_pvals_parallel(matrix=matrix, method=method)
+                left_interval, right_interval = np.percentile(
+                    np.array(bs_samples),
+                    [100 * (1 - self.confidence_interval) / 2, 100 * (1 - (1 - self.confidence_interval) / 2)]
+                )
+                pvalues[pvalues > 0.5] = 1 - pvalues[pvalues > 0.5]  # we are doing one-sided test
+                results[method] = {
+                    'correlations': self.correlations_by_method[method],
+                    'pvalues': pvalues,
+                    'confidence_interval': (left_interval, right_interval)
+                }
+        if len(results) > 1:
+            with timer('Merging p values...'):
+                pvals = merge_p_values(
+                    *[result['pvalues'] for result in results.values()],
+                    self.common_edges
+                )
+        else:
+            pvals = next(iter(results.values()))['pvalues']
+        if self.bh:
+            with timer('Performing Benjamini-Hochberg correction...'):
+                pvals = benjamini_hochberg(pvals, self.common_edges)
+
+        self.results = {
+            'by_method': results,
+            'pvals': pvals
+        }
+        rejected_edges = Counter()
+        accepted_edges = Counter()
+        network = nx.Graph()
+        for method, result in results.items():
+            correlations = result['correlations']
+            left_interval, right_interval = result['confidence_interval']
+            method_pvalues = result['pvalues']
+            for i in range(correlations.shape[0]):
+                for j in range(correlations.shape[1]):
+                    if i != j and i < j and ((i, j) in self.common_edges or (j, i) in self.common_edges):
+                        if (
+                                pvals[i, j] < self.p_value_threshold
+                                and (left_interval < correlations[i, j] < right_interval)
+                        ):
+                            left_otu_id = self.indice_to_id[i]
+                            right_otu_id = self.indice_to_id[j]
+                            if not network.has_edge(left_otu_id, right_otu_id):
+                                network.add_edge(
+                                    left_otu_id, right_otu_id,
+                                    pvalue=pvals[i, j],
+                                )
+                                accepted_edges[method] += 1
+                            network[left_otu_id][right_otu_id][f'{method}_weight'] = correlations[i, j]
+                            network[left_otu_id][right_otu_id][f'{method}_pvalue'] = method_pvalues[i, j]
+                        else:
+                            rejected_edges[method] += 1
+        print('Accepted edges:', accepted_edges)
+        print('Rejected edges:', rejected_edges)
+
+        return network
+
+    @staticmethod
+    def select_edges(matrix: np.ndarray, method: str, max_edges=100000) -> Tuple[Set[Tuple[int, int]], int, int]:
+        if method == 'kullback-leibler':
+            pos_edges = [
+                (val, i, j) for val, i, j in sorted(matrix_iter(matrix), key=lambda x: x[0])[:max_edges // 2]
+            ]
+            neg_edges = [
+                (val, i, j) for val, i, j in
+                sorted(matrix_iter(matrix), key=lambda x: x[0], reverse=True)[:max_edges // 2]
+            ]
+            print(pos_edges[:10])
+            print(neg_edges[:10])
+
+            left = max(x[0] for x in pos_edges)
+            right = min(x[0] for x in neg_edges)
+            return set([(i, j) for _, i, j in pos_edges] + [(i, j) for _, i, j in neg_edges]), left, right
+        else:
+            left = 0
+            right = 0
+            edges = set()
+            for val, i, j in sorted(matrix_iter(matrix), key=lambda x: abs(x[0]), reverse=True):
+                if len(edges) < max_edges and (j, i) not in edges:
+                    edges.add((i, j))
+                    if val < 0 and val < left:
+                        left = val
+                    if val > 0 and val > right:
+                        right = val
+            return edges, left, right
+
+    def compute_pvals_parallel(self, matrix: np.ndarray, method: str):
+        def prepare_proc_iters(n_iter, n_jobs):
+            iters = [n_iter // n_jobs for _ in range(n_jobs)]
+            iters[-1] += n_iter - sum(iters)
+            return iters
+
+        executor = Parallel(n_jobs=self.n_jobs, verbose=10)
+        delated_fun = delayed(reboot)
+        tasks = (
+            delated_fun(matrix, proc_iters, method, self.common_edges)
+            for proc_iters in prepare_proc_iters(self.n_iter, self.n_jobs)
+        )
+
+        current_count = 0
+        perm_means = np.zeros((matrix.shape[1], matrix.shape[1]))
+        perm_variances = np.zeros((matrix.shape[1], matrix.shape[1]))
+        bs_means = np.zeros((matrix.shape[1], matrix.shape[1]))
+        bs_variances = np.zeros((matrix.shape[1], matrix.shape[1]))
+        samples = []
+        for result in executor(tasks):
+            curr_perm_means, curr_perm_variances, curr_bs_means, curr_bs_variances, count, proc_samples = result
+            perm_means, perm_variances = update_mean_variance_parallel(
+                current_count, perm_means, perm_variances,
+                count, curr_perm_means, curr_perm_variances
+            )
+            bs_means, bs_variances = update_mean_variance_parallel(
+                current_count, bs_means, bs_variances,
+                count, curr_bs_means, curr_bs_variances
+            )
+            current_count += count
+            samples.extend(proc_samples)
+        perm_variances /= current_count
+        bs_variances /= current_count
+        assert current_count == self.n_iter
+
+        return compute_p_value(perm_means, perm_variances, bs_means, bs_variances), samples
 
 
 if __name__ == '__main__':
@@ -162,12 +219,9 @@ if __name__ == '__main__':
     inp, n_threads, output = sys.argv[1:4]
     otu_table = OTUTable.from_tsv(inp, sample_rows=False)
     start_time = time.time()
-    methods = ['kullback-leibler', 'spearman']
-    network = infer_network(
-        otu_table=otu_table, n_iter=2, methods=methods,
-        p_value_threshold=0.05, n_jobs=int(n_threads),
-        tmp_dir=os.path.join(os.path.split(inp)[0], 'temp_dir')
-    )
+    methods = ['spearman', 'kullback-leibler']
+    conet = CoNet(otu_table, n_jobs=int(n_threads), n_iter=1000)
+    network = conet.run()
     print('Took', time.time() - start_time)
     with open(output, 'w') as handle:
         writer = csv.writer(handle, delimiter='\t')
