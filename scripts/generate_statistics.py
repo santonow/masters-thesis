1bc55798321f0e7fdb660855cfb1031cafbeb545
santonow <stas.antonowicz@gmail.com>
2022-07-17 10:38:50 +0200

    Fix a bug.

--- scripts/generate_statistics.py
+++ scripts/generate_statistics.py
@@ -898,8 +898,8 @@ if __name__ == "__main__":
         graphs_by_method[network_name] = graph
         stats_by_method[network_name] = stats
     lima_mendez_graph = nx.Graph()
-    for head, tail in predicted_interactions:
-        lima_mendez_graph.add_edge(head, tail)
+    for (head, tail), attrs in predicted_interactions:
+        lima_mendez_graph.add_edge(head, tail, **attrs)
     stats_by_method['Lima-Mendez TARA interactome'] = prepare_stats(lima_mendez_graph)
     graphs_by_method['Lima-Mendez TARA interactome'] = lima_mendez_graph
     graphs = []
