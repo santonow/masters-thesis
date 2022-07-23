10bb73c0d1b2017ea81fde52bb0fee843106d154
santonow <stas.antonowicz@gmail.com>
2022-07-23 13:32:26 +0200

    Replacing sample_visualization_file.json with data.json. Applying prettier to index.ts.

--- visualization/index.ts
+++ visualization/index.ts
@@ -1,5 +1,5 @@
 import Sigma from "sigma";
-import { Coordinates, EdgeDisplayData, NodeDisplayData } from "sigma/types";
+import {Coordinates, EdgeDisplayData, NodeDisplayData} from "sigma/types";
 import Graph from "graphology";
 
 import data from "./data.json";
@@ -11,48 +11,48 @@ let sources: Set<string> = new Set<string>();
 
 let node2Network: { [node: string]: Set<string> } = {};
 graph.forEachNode((node: string) => {
-  node2Network[node] = new Set<string>();
+    node2Network[node] = new Set<string>();
 });
 
 graph.forEachEdge(
-  (edge, attributes, source, target, sourceAttributes, targetAttributes) => {
-    attributes.sources.forEach((network: string) => {
-      node2Network[source].add(network);
-      node2Network[target].add(network);
-    });
-  }
+    (edge, attributes, source, target, sourceAttributes, targetAttributes) => {
+        attributes.sources.forEach((network: string) => {
+            node2Network[source].add(network);
+            node2Network[target].add(network);
+        });
+    }
 );
 
 graph.forEachEdge((edge: string) => {
-  const sign: string = graph.getEdgeAttribute(edge, "sign");
-  if (sign === "+") {
-    graph.setEdgeAttribute(edge, "color", "#99c2ff");
-  } else if (sign === "-") {
-    graph.setEdgeAttribute(edge, "color", "#ff9999");
-  } else {
-    graph.setEdgeAttribute(edge, "color", "#bfbfbf");
-  }
-  graph.getEdgeAttributes(edge).sources.forEach((source: string) => {
-    sources.add(source);
-  });
+    const sign: string = graph.getEdgeAttribute(edge, "sign");
+    if (sign === "+") {
+        graph.setEdgeAttribute(edge, "color", "#99c2ff");
+    } else if (sign === "-") {
+        graph.setEdgeAttribute(edge, "color", "#ff9999");
+    } else {
+        graph.setEdgeAttribute(edge, "color", "#bfbfbf");
+    }
+    graph.getEdgeAttributes(edge).sources.forEach((source: string) => {
+        sources.add(source);
+    });
 });
 
 const buttons = document.getElementById("buttons");
 let sourceButtons: Record<string, HTMLButtonElement> = {};
 sources.forEach((source: string) => {
-  const button = document.createElement("button") as HTMLButtonElement;
-  button.id = source;
-  button.textContent = source;
-  buttons?.appendChild(button);
-  sourceButtons[source] = button;
+    const button = document.createElement("button") as HTMLButtonElement;
+    button.id = source;
+    button.textContent = source;
+    buttons?.appendChild(button);
+    sourceButtons[source] = button;
 });
 
 function setClicked(button: HTMLButtonElement) {
-  button.style.background = "#d4d4d4";
+    button.style.background = "#d4d4d4";
 }
 
 function setUnclicked(button: HTMLButtonElement) {
-  button.style.background = "#ffffff";
+    button.style.background = "#ffffff";
 }
 
 // Retrieve some useful DOM elements:
@@ -60,241 +60,239 @@ const container = document.getElementById("sigma-container") as HTMLElement;
 
 const searchInput = document.getElementById("search-input") as HTMLInputElement;
 const searchSuggestions = document.getElementById(
-  "suggestions"
+    "suggestions"
 ) as HTMLDataListElement;
 
 // Instantiate sigma:
 const renderer = new Sigma(graph, container, {
-  minCameraRatio: 0.1,
-  maxCameraRatio: 10,
+    minCameraRatio: 0.1,
+    maxCameraRatio: 10,
 });
 
 // Type and declare internal state:
 interface State {
-  hoveredNode?: string;
-  searchQuery: string;
+    hoveredNode?: string;
+    searchQuery: string;
 
-  // State derived from query:
-  selectedNode?: string;
-  suggestions?: Set<string>;
+    // State derived from query:
+    selectedNode?: string;
+    suggestions?: Set<string>;
 
-  // State derived from hovered node:
-  hoveredNeighbors?: Set<string>;
+    // State derived from hovered node:
+    hoveredNeighbors?: Set<string>;
 
-  // State derived from edge visualization options.
-  selectedNetworks?: Set<string>;
+    // State derived from edge visualization options.
+    selectedNetworks?: Set<string>;
 }
 
 const state: State = {
-  searchQuery: "",
-  selectedNetworks: new Set<string>(["consensus"]),
+    searchQuery: "",
+    selectedNetworks: new Set<string>(["consensus"]),
 };
 
 // Feed the datalist autocomplete values:
 searchSuggestions.innerHTML = graph
-  .nodes()
-  .map(
-    (node) =>
-      `<option value="${graph.getNodeAttribute(node, "label")}"></option>`
-  )
-  .join("\n");
+    .nodes()
+    .map(
+        (node) =>
+            `<option value="${graph.getNodeAttribute(node, "label")}"></option>`
+    )
+    .join("\n");
 
 // Actions:
 function setSearchQuery(query: string) {
-  state.searchQuery = query;
+    state.searchQuery = query;
 
-  if (searchInput.value !== query) searchInput.value = query;
+    if (searchInput.value !== query) searchInput.value = query;
 
-  if (query) {
-    const lcQuery = query.toLowerCase();
-    const suggestions = graph
-      .nodes()
-      .map((n) => ({
-        id: n,
-        label: graph.getNodeAttribute(n, "label") as string,
-        lineage: graph.getNodeAttribute(n, "lineage") as Array<string>,
-      }))
-      .filter(
-        ({ label, lineage }) =>
-          label.toLowerCase().includes(lcQuery) ||
-          lineage.some((taxon: string) => taxon.toLowerCase().includes(lcQuery))
-      );
+    if (query) {
+        const lcQuery = query.toLowerCase();
+        const suggestions = graph
+            .nodes()
+            .map((n) => ({
+                id: n,
+                label: graph.getNodeAttribute(n, "label") as string,
+                lineage: graph.getNodeAttribute(n, "lineage") as Array<string>,
+            }))
+            .filter(
+                ({label, lineage}) =>
+                    label.toLowerCase().includes(lcQuery) ||
+                    lineage.some((taxon: string) => taxon.toLowerCase().includes(lcQuery))
+            );
 
-    if (suggestions.length === 1 && suggestions[0].label === query) {
-      state.selectedNode = suggestions[0].id;
-      state.suggestions = undefined;
+        if (suggestions.length === 1 && suggestions[0].label === query) {
+            state.selectedNode = suggestions[0].id;
+            state.suggestions = undefined;
 
-      const nodePosition = renderer.getNodeDisplayData(
-        state.selectedNode
-      ) as Coordinates;
-      renderer.getCamera().animate(nodePosition, {
-        duration: 500,
-      });
+            const nodePosition = renderer.getNodeDisplayData(
+                state.selectedNode
+            ) as Coordinates;
+            renderer.getCamera().animate(nodePosition, {
+                duration: 500,
+            });
+        } else {
+            state.selectedNode = undefined;
+            state.suggestions = new Set(suggestions.map(({id}) => id));
+        }
+    } else {
+        state.selectedNode = undefined;
+        state.suggestions = undefined;
     }
-    else {
-      state.selectedNode = undefined;
-      state.suggestions = new Set(suggestions.map(({ id }) => id));
-    }
-  }
-  else {
-    state.selectedNode = undefined;
-    state.suggestions = undefined;
-  }
 
-  renderer.refresh();
+    renderer.refresh();
 }
 
 function setHoveredNode(node?: string) {
-  if (node) {
-    state.hoveredNode = node;
-    state.hoveredNeighbors = neighborsInSelectedNetworks(node);
-    console.log(node2Network[node]);
-  } else {
-    state.hoveredNode = undefined;
-    state.hoveredNeighbors = undefined;
-  }
+    if (node) {
+        state.hoveredNode = node;
+        state.hoveredNeighbors = neighborsInSelectedNetworks(node);
+        console.log(node2Network[node]);
+    } else {
+        state.hoveredNode = undefined;
+        state.hoveredNeighbors = undefined;
+    }
 
-  renderer.refresh();
+    renderer.refresh();
 }
 
 function setSelectedNetwork(network?: string) {
-  let selected_networks: Set<string> = new Set<string>(state.selectedNetworks);
-  if (selected_networks.has(network)) {
-    selected_networks.delete(network);
-  } else {
-    selected_networks.add(network);
-  }
-  state.selectedNetworks = selected_networks;
+    let selected_networks: Set<string> = new Set<string>(state.selectedNetworks);
+    if (selected_networks.has(network)) {
+        selected_networks.delete(network);
+    } else {
+        selected_networks.add(network);
+    }
+    state.selectedNetworks = selected_networks;
 
-  renderer.refresh();
+    renderer.refresh();
 }
 
 // Bind search input interactions:
 searchInput.addEventListener("input", () => {
-  setSearchQuery(searchInput.value || "");
+    setSearchQuery(searchInput.value || "");
 });
 searchInput.addEventListener("blur", () => {
-  setSearchQuery("");
+    setSearchQuery("");
 });
 
 // Bind graph interactions:
-renderer.on("enterNode", ({ node }) => {
-  setHoveredNode(node);
+renderer.on("enterNode", ({node}) => {
+    setHoveredNode(node);
 });
 renderer.on("leaveNode", () => {
-  setHoveredNode(undefined);
+    setHoveredNode(undefined);
 });
 
 function setClickedState(button: HTMLButtonElement, source: string) {
-  if (state.selectedNetworks.has(source)) {
-    setClicked(button);
-  } else {
-    setUnclicked(button);
-  }
+    if (state.selectedNetworks.has(source)) {
+        setClicked(button);
+    } else {
+        setUnclicked(button);
+    }
 }
 
 Object.entries(sourceButtons).forEach(([source, button]) => {
-  setClickedState(button, source);
-  button.addEventListener("click", () => {
-    setSelectedNetwork(source);
     setClickedState(button, source);
-    console.log(`Clicked ${source}`);
-    console.log(state.selectedNetworks);
-  });
+    button.addEventListener("click", () => {
+        setSelectedNetwork(source);
+        setClickedState(button, source);
+        console.log(`Clicked ${source}`);
+        console.log(state.selectedNetworks);
+    });
 });
 
 function neighborsInSelectedNetworks(node: string) {
-  let neighbors: Set<string> = new Set<string>();
-  if (node) {
-    graph.neighbors(node).forEach((neighbor: string) => {
-      let edge: string;
-      if (graph.hasEdge(node, neighbor)) {
-        edge = graph.edge(node, neighbor);
-      } else {
-        edge = graph.edge(neighbor, node);
-      }
-      const edge_sources: Set<string> = new Set<string>(
-        graph.getEdgeAttribute(edge, "sources")
-      );
-      if (showEdge(state.selectedNetworks, edge_sources)) {
-        neighbors.add(neighbor);
-      }
-    });
-  }
-  return neighbors;
+    let neighbors: Set<string> = new Set<string>();
+    if (node) {
+        graph.neighbors(node).forEach((neighbor: string) => {
+            let edge: string;
+            if (graph.hasEdge(node, neighbor)) {
+                edge = graph.edge(node, neighbor);
+            } else {
+                edge = graph.edge(neighbor, node);
+            }
+            const edge_sources: Set<string> = new Set<string>(
+                graph.getEdgeAttribute(edge, "sources")
+            );
+            if (showEdge(state.selectedNetworks, edge_sources)) {
+                neighbors.add(neighbor);
+            }
+        });
+    }
+    return neighbors;
 }
 
 
 function showNode(selectedNetworks: Set<string>, nodeSources: Set<string>) {
-  for (let elem of nodeSources.values()) {
-    if (selectedNetworks.has(elem)) {
-      return true;
+    for (let elem of nodeSources.values()) {
+        if (selectedNetworks.has(elem)) {
+            return true;
+        }
     }
-  }
-  return false;
+    return false;
 }
 
 renderer.setSetting("nodeReducer", (node, data) => {
-  const res: Partial<NodeDisplayData> = { ...data };
+    const res: Partial<NodeDisplayData> = {...data};
 
-  if (state.hoveredNeighbors && state.hoveredNeighbors.has(node)) {
-    res.highlighted = true;
-  }
-  if (!showNode(state.selectedNetworks, node2Network[node])) {
-    res.hidden = true;
-  }
-  if (
-    state.hoveredNeighbors &&
-    !state.hoveredNeighbors.has(node) &&
-    state.hoveredNode !== node
-  ) {
-    res.hidden = true;
-  } else if (state.suggestions && !state.suggestions.has(node)) {
-    res.hidden = true;
-  }
+    if (state.hoveredNeighbors && state.hoveredNeighbors.has(node)) {
+        res.highlighted = true;
+    }
+    if (!showNode(state.selectedNetworks, node2Network[node])) {
+        res.hidden = true;
+    }
+    if (
+        state.hoveredNeighbors &&
+        !state.hoveredNeighbors.has(node) &&
+        state.hoveredNode !== node
+    ) {
+        res.hidden = true;
+    } else if (state.suggestions && !state.suggestions.has(node)) {
+        res.hidden = true;
+    }
 
-  return res;
+    return res;
 });
 
 function showEdge(selectedNetworks: Set<string>, edgeSources: Set<string>) {
-  if (edgeSources.has("consensus") && selectedNetworks.has("consensus")) {
-    return true;
-  }
-  for (let elem of edgeSources.values()) {
-    if (elem != "consensus" && !selectedNetworks.has(elem)) {
-      return false;
+    if (edgeSources.has("consensus") && selectedNetworks.has("consensus")) {
+        return true;
     }
-  }
-  return true;
+    for (let elem of edgeSources.values()) {
+        if (elem != "consensus" && !selectedNetworks.has(elem)) {
+            return false;
+        }
+    }
+    return true;
 }
 
 
-renderer.setSetting("edgeReducer", (edge, data) => {
-  const res: Partial<EdgeDisplayData> = { ...data };
+renderer.setSetting("edgeReducer", (edge: string, data) => {
+    const res: Partial<EdgeDisplayData> = {...data};
 
-  if (
-    !showEdge(
-      state.selectedNetworks,
-      new Set<string>(graph.getEdgeAttribute(edge, "sources"))
-    )
-  ) {
-    res.hidden = true;
-  }
-  if (state.hoveredNode && !graph.hasExtremity(edge, state.hoveredNode)) {
-    res.hidden = true;
-  }
-  if (
-    state.suggestions &&
-    (!state.suggestions.has(graph.source(edge)) ||
-      !state.suggestions.has(graph.target(edge)))
-  ) {
-    res.hidden = true;
-  }
-  return res;
+    if (
+        !showEdge(
+            state.selectedNetworks,
+            new Set<string>(graph.getEdgeAttribute(edge, "sources"))
+        )
+    ) {
+        res.hidden = true;
+    }
+    if (state.hoveredNode && !graph.hasExtremity(edge, state.hoveredNode)) {
+        res.hidden = true;
+    }
+    if (
+        state.suggestions &&
+        (!state.suggestions.has(graph.source(edge)) ||
+            !state.suggestions.has(graph.target(edge)))
+    ) {
+        res.hidden = true;
+    }
+    return res;
 });
 
 const camera = renderer.getCamera();
 
 camera.setState({
-  angle: 0.3,
+    angle: 0.3,
 });
